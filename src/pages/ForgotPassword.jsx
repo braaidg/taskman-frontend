4b7ec6edb8e21c6dc3378e037e7321235aaaae82
Braian Gomez <braiandenisgomez@gmail.com>
2022-12-12 14:03:13 -0300

    Added forgot password and register pages

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,5 +1,51 @@
+import { Link } from "react-router-dom";
+
 const ForgotPassword = () => {
-  return <div>ForgotPassword</div>;
+  return (
+    <>
+      <h1 className="text-sky-600 font-black text-6xl capitalize">
+        Recover your account and don't lose your{" "}
+        <span className="text-slate-700">projects</span>
+      </h1>
+
+      <form className="my-10 bg-white shadow rounded-lg px-10 py-10">
+        <div className="my-5">
+          <label
+            htmlFor="email"
+            className="uppercase text-gray-600 block text-xl font-bold"
+          >
+            Email
+          </label>
+          <input
+            id="email"
+            type="email"
+            placeholder="Registered email"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+
+        <input
+          type="submit"
+          value="Send instructions"
+          className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
+        />
+      </form>
+      <nav className="lg:flex lg:justify-between">
+        <Link
+          to="/"
+          className="block text-center my-5 text-slate-500 uppercase text-sm"
+        >
+          Already have an account? Log in!
+        </Link>
+        <Link
+          to="/register"
+          className="block text-center my-5 text-slate-500 uppercase text-sm"
+        >
+          Don't have an account? Register now!
+        </Link>
+      </nav>
+    </>
+  );
 };
 
 export default ForgotPassword;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,93 @@
+import { Link } from "react-router-dom";
+
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <>
+      <h1 className="text-sky-600 font-black text-6xl capitalize">
+        Register your account and manage your{" "}
+        <span className="text-slate-700">projects</span>
+      </h1>
+
+      <form className="my-10 bg-white shadow rounded-lg px-10 py-10">
+        <div className="my-5">
+          <label
+            htmlFor="name"
+            className="uppercase text-gray-600 block text-xl font-bold"
+          >
+            Name
+          </label>
+          <input
+            id="name"
+            type="name"
+            placeholder="Your name"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+        <div className="my-5">
+          <label
+            htmlFor="email"
+            className="uppercase text-gray-600 block text-xl font-bold"
+          >
+            Email
+          </label>
+          <input
+            id="email"
+            type="email"
+            placeholder="Registered email"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+        <div className="my-5">
+          <label
+            htmlFor="password"
+            className="uppercase text-gray-600 block text-xl font-bold"
+          >
+            Password
+          </label>
+          <input
+            id="password"
+            type="password"
+            placeholder="Registered password"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+        <div className="my-5">
+          <label
+            htmlFor="repeat-password"
+            className="uppercase text-gray-600 block text-xl font-bold"
+          >
+            Repeat your password
+          </label>
+          <input
+            id="repeat-password"
+            type="password"
+            placeholder="Repeat your password"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+
+        <input
+          type="submit"
+          value="Register"
+          className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
+        />
+      </form>
+      <nav className="lg:flex lg:justify-between">
+        <Link
+          to="/"
+          className="block text-center my-5 text-slate-500 uppercase text-sm"
+        >
+          Already have an account? Log in!
+        </Link>
+        <Link
+          to="/forgot-password"
+          className="block text-center my-5 text-slate-500 uppercase text-sm"
+        >
+          Forgot my password
+        </Link>
+      </nav>
+    </>
+  );
 };
 
 export default Register;
