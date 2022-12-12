abaa49c9293924c432c223fab77ab3004b5a4857
Braian Gomez <braiandenisgomez@gmail.com>
2022-12-12 13:41:55 -0300

    Created routes and basic components with tailwind

---
+++ src/pages/Login.jsx
@@ -0,0 +1,48 @@
+const Login = () => {
+  return (
+    <>
+      <h1 className="text-sky-600 font-black text-6xl capitalize">
+        Log in and manage your <span className="text-slate-700">projects</span>
+      </h1>
+
+      <form className="my-10 bg-white shadow rounded-lg px-10 py-10">
+        <div className="my-5">
+          <label
+            htmlFor="email"
+            className="uppercase text-gray-600 block text-xl font-bold"
+          >
+            Email
+          </label>
+          <input
+            id="email"
+            type="email"
+            placeholder="Registered email"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+        <div className="my-5">
+          <label
+            htmlFor="password"
+            className="uppercase text-gray-600 block text-xl font-bold"
+          >
+            Password
+          </label>
+          <input
+            id="password"
+            type="password"
+            placeholder="Registered password"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+
+        <input
+          type="submit"
+          value="Log in"
+          className="bg-sky-700 mb-5 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors"
+        />
+      </form>
+    </>
+  );
+};
+
+export default Login;
