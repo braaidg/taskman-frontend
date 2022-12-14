e97760c3830a9f3d2c07ac851e349f99cd6e4671
Braian Gomez <braiandenisgomez@gmail.com>
2022-12-14 16:02:18 -0300

    Added validations and set token in localstorage

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,53 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Alert from "../components/Alert";
+import clientAxios from "../config/clientAxios";
 
 const Login = () => {
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const [alert, setAlert] = useState({});
+
+  const { email, password } = formData;
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (Object.values(formData).includes("")) {
+      setAlert({ msg: "All fields are required", error: true });
+      return;
+    }
+
+    try {
+      const { data } = await clientAxios.post("/users/login", {
+        email,
+        password,
+      });
+      setAlert({});
+      localStorage.setItem("token", data.token);
+    } catch (error) {
+      console.log(error.response.data.msg);
+      setAlert({ msg: error.response.data.msg, error: true });
+    }
+  };
+
+  const { msg } = alert;
+
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl capitalize">
         Log in and manage your <span className="text-slate-700">projects</span>
       </h1>
 
-      <form className="my-10 bg-white shadow rounded-lg px-10 py-10">
+      {msg && <Alert alert={alert} />}
+
+      <form
+        onSubmit={handleSubmit}
+        className="my-10 bg-white shadow rounded-lg px-10 py-10"
+      >
         <div className="my-5">
           <label
             htmlFor="email"
@@ -19,6 +59,9 @@ const Login = () => {
             id="email"
             type="email"
             placeholder="Registered email"
+            name="email"
+            value={email}
+            onChange={handleInputChange}
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
           />
         </div>
@@ -32,7 +75,10 @@ const Login = () => {
           <input
             id="password"
             type="password"
+            name="password"
             placeholder="Registered password"
+            value={password}
+            onChange={handleInputChange}
             className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
           />
         </div>
