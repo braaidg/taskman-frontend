10966ef5298462ec33ece3576ed2f3ac2e8ec1e7
Braian Gomez <braiandenisgomez@gmail.com>
2022-12-17 21:21:01 -0300

    Created protected route layout

---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,16 @@
+import { Navigate, Outlet } from "react-router-dom"
+import useAuth from "../hooks/useAuth"
+
+
+const ProtectedRoute = () => {
+  const {auth, loading} = useAuth();
+  if(loading) return "Loading";
+
+  return (
+    <>
+      {auth._id ? <Outlet/> : <Navigate to="/" />}
+    </>
+  )
+}
+
+export default ProtectedRoute
