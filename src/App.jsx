73b129ee2ac2997507670de14d0a68ebca4849cc
Braian Gomez <braiandenisgomez@gmail.com>
2022-12-21 18:00:07 -0300

    Added projects provider to App

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import ForgotPassword from "./pages/ForgotPassword";
 import NewPassword from "./pages/NewPassword";
 import ConfirmAccount from "./pages/ConfirmAccount";
 import { AuthProvider } from "./context/AuthProvider";
+import { ProjectsProvider } from "./context/ProjectsProvider";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Projects from "./pages/Projects";
 import NewProject from "./pages/NewProject";
@@ -14,20 +15,22 @@ function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          <Route path="/" element={<AuthLayout />}>
-            <Route index element={<Login />} />
-            <Route path="register" element={<Register />} />
-            <Route path="forgot-password" element={<ForgotPassword />} />
-            <Route path="forgot-password/:token" element={<NewPassword />} />
-            <Route path="confirm-account/:id" element={<ConfirmAccount />} />
-          </Route>
+        <ProjectsProvider>
+          <Routes>
+            <Route path="/" element={<AuthLayout />}>
+              <Route index element={<Login />} />
+              <Route path="register" element={<Register />} />
+              <Route path="forgot-password" element={<ForgotPassword />} />
+              <Route path="forgot-password/:token" element={<NewPassword />} />
+              <Route path="confirm-account/:id" element={<ConfirmAccount />} />
+            </Route>
 
-          <Route path="/projects" element={<ProtectedRoute/>}>
-            <Route index element={<Projects/>} />
-            <Route path="create-project" element={<NewProject/>} />
-          </Route>
-        </Routes>
+            <Route path="/projects" element={<ProtectedRoute />}>
+              <Route index element={<Projects />} />
+              <Route path="create-project" element={<NewProject />} />
+            </Route>
+          </Routes>
+        </ProjectsProvider>
       </AuthProvider>
     </BrowserRouter>
   );
