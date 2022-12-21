317e508d3820602a8c02fcce297f0f20422a41ac
Braian Gomez <braiandenisgomez@gmail.com>
2022-12-21 18:58:02 -0300

    Finished project form

--- src/components/ProjectForm.jsx
+++ src/components/ProjectForm.jsx
@@ -1,13 +1,41 @@
 import { useState } from "react";
+import useProjects from "../hooks/useProjects";
+import Alert from "../components/Alert";
+
+const currentDay = new Intl.DateTimeFormat("fr-CA").format();
 
 const ProjectForm = () => {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
-  const [finishDate, setFinishDate] = useState("");
+  const [finishDate, setFinishDate] = useState(currentDay);
   const [client, setClient] = useState("");
 
+  const { showAlert, alert, submitProject } = useProjects();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if ([name, description, finishDate, client].includes("")) {
+      showAlert({ msg: "All fields are required", error: true });
+      return;
+    }
+    await submitProject({ name, description, finishDate, client });
+
+    setName("");
+    setDescription("");
+    setFinishDate("");
+    setClient("");
+  };
+
+  const { msg } = alert;
+
   return (
-    <form className="bg-white py-10 px-5 md:w-1/2 rounded-lg shadow">
+    <form
+      onSubmit={handleSubmit}
+      className="bg-white py-10 px-5 md:w-1/2 rounded-lg shadow"
+    >
+      {msg && <Alert alert={alert} />}
+
       <div className="mb-5">
         <label
           htmlFor="name"
@@ -50,6 +78,7 @@ const ProjectForm = () => {
         <input
           id="finishing-date"
           type="date"
+          min={currentDay}
           className="border w-full p-2 mt-2 placeholder-gray-400 rounded-md"
           value={finishDate}
           onChange={(e) => setFinishDate(e.target.value)}
