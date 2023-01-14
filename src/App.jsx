efd2ba434a6e679f39cb52f6749d74b51ed3997a
Braian Gomez <braiandenisgomez@gmail.com>
2023-01-14 20:10:40 -0300

    Added new collaborator route

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import Projects from "./pages/Projects";
 import NewProject from "./pages/NewProject";
 import Project from "./pages/Project";
 import EditProject from "./pages/EditProject";
+import NewCollaborator from "./pages/NewCollaborator";
 
 function App() {
   return (
@@ -38,6 +39,10 @@ function App() {
               <Route path="/projects" element={<ProtectedRoute />}>
                 <Route index element={<Projects />} />
                 <Route path="create-project" element={<NewProject />} />
+                <Route
+                  path="new-collaborator/:id"
+                  element={<NewCollaborator />}
+                />
                 <Route path=":id" element={<Project />} />
                 <Route path="edit/:id" element={<EditProject />} />
               </Route>
